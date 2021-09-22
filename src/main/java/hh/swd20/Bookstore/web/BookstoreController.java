08154b8f96aa5057977d90515a0fd37707fda480
Miro Riikonen <miro.riikonen@gmail.com>
2021-09-22 16:48:50 +0300

    tehtävien jälkeinen päivitys githubiin

--- src/main/java/hh/swd20/Bookstore/web/BookstoreController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookstoreController.java
@@ -1,16 +1,49 @@
 package hh.swd20.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.swd20.Bookstore.domain.Book;
+import hh.swd20.Bookstore.domain.BookRepository;
+
 @Controller
 public class BookstoreController {
-	
-	// http://localhost:8080/index
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String index() {
+	@Autowired
+	private BookRepository repository;
+	// http://localhost:8080/booklist
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		
 		return "bookstore"; // bookstore.html
+	}
+	
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook"; // addbook.html
+	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
+		return "editbook"; // editbook.html
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist"; // booklist.html
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist"; // booklist.html
 	}
 
 }
